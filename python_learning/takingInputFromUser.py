17695eb344dcc39579d3db3f1e459ee6d0defa62
YogDPatil <ydpatil.it@gmail.com>
2025-06-06 22:45:16 +0530

    take input from user and convert into int

---
+++ python_learning/takingInputFromUser.py
@@ -0,0 +1,9 @@
+
+num1= input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+print(type(num1)) #string-> always consider as string
+
+# convert string into int
+print(int(num1)+int(num2))
+print(type(int(num1)))
